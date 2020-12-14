3cee0f9163203bdc4be492cc30ee8eee48011235
foggynight <foggynight709@gmail.com>
2020-12-13 20:54:27 -0330

    Remove SPDX license identifier

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: GPL-2.0
-// Copyright (C) 2020 Robert Coffey
-/**
- * --- rain ---
+/* --- rain ---
  *
  * Interactive rain simulation.
+ *
+ * Copyright (C) 2020 Robert Coffey
+ * Licensed under the GNU GPLv2
  */
 
 #include <chrono>
